b78187eb675416c0e6551d26d80205922cf363bb
Ataraxia ‎ <gabrielvincentreyes13@gmail.com>
2026-03-23 17:09:54 +0800

    Added the game logic     added a for loop for the rounds and a simple function calls after each match to start a new set of rounds.     scores are also printed and its resetted back to 0 whenever a new match starts

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 let humanScore = 0
 let computerScore = 0
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
 
 function getHumanChoice() {
     let human = prompt("Whats your choice? Rock, Paper, or Scissors?")
@@ -9,7 +7,7 @@ function getHumanChoice() {
 }
 
 function getComputerChoice() {
-    i = Math.floor(Math.random() *3)+1
+    let i = Math.floor(Math.random() *3)+1
     if (i == 1) {
         return "Rock"
     } else if (i == 2) {
@@ -19,12 +17,12 @@ function getComputerChoice() {
     }
 }
 
-function playRound(human, computer) {
-    const h = human.toLowerCase();
-    const c = computer.toLowerCase();
+function playRound(humanChoice, computerChoice) {
+    const h = humanChoice.toLowerCase();
+    const c = computerChoice.toLowerCase();
 
     if (h === c) { 
-        return "Tie!"
+        return "Tie! you got lucky!"
     }
 
     if (
@@ -32,18 +30,63 @@ function playRound(human, computer) {
         (h === "paper" && c === "rock") ||
         (h ==="scissors" && c === "paper")
     )   {
-        return 'Your win!'
+        humanScore++;
+        return 'Your win!...smh';
     }
 
     if (h !== "rock" && h !== "paper" && h !== "scissors") {
         console.log("Please enter something valid!")
-        let newChoice = getHumanChoice();
-        return playRound(newChoice, computer);
+        let humanChoice = getHumanChoice();
+        return playRound(humanChoice, computerChoice);
     }
 
-    return "You lose:D"
+    computerScore++;
+    return `You lose:D ${c} beats ${h}, try again broski`;
 }
 
-console.log(playRound(humanChoice, computerChoice))
-console.log(`Player chose: ${humanChoice} | Computer chose: ${computerChoice}`);
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        console.log(`~~ Round: ${i + 1} ~~`)
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        console.log(playRound(humanChoice, computerChoice));
+        console.log(`Player Score: ${humanScore} |  Computers Score: ${computerScore}`);
+    }
+
+    if (humanScore > computerScore) {
+        alert(`You won the match! Final Score: ${humanScore} to ${computerScore}`);
+    } else if (computerScore > humanScore) {
+        alert(`The AI outsmarted you. Final Score: ${computerScore} to ${humanScore}`);
+    } else {
+        alert("It's a draw! Nobody wins this time.");
+    }
+
+    if (confirm("Wanna go again scaredy?")) {
+        humanScore = 0
+        computerScore = 0
+        playGame()
+    }
+
+    else {
+        alert("Okay~ bye bye!")
+    }
+}
+
+if (confirm("You ready to go against my super smart, super advanced AI?")) {
+    playGame();
+} else {
+
+    if (confirm("Are you sure sure?")) {
+        alert("Scaredy cat. tsk. Ctrl+R when you grow a spine!")
+    } 
+    
+    else {
+        alert("Not bad!")
+        playGame()
+    }
+}
+
+
+
+
 
