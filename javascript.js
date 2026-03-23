e28540dd26af1d6eabeb9e9ef3888822f4440720
Ataraxia ‎ <gabrielvincentreyes13@gmail.com>
2026-03-23 12:38:25 +0800

    added the logic     checks  for winnable scenario, then handles invalid entries. also made it so it can handle lowercase and uppercase.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
+let humanScore = 0
+let computerScore = 0
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+function getHumanChoice() {
+    let human = prompt("Whats your choice? Rock, Paper, or Scissors?")
+    return human
+}
+
 function getComputerChoice() {
     i = Math.floor(Math.random() *3)+1
     if (i == 1) {
@@ -9,18 +19,31 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let human = prompt("Whats your choice? (1)Rock, (2)Paper, or (3)Scissors?")
+function playRound(human, computer) {
+    const h = human.toLowerCase();
+    const c = computer.toLowerCase();
 
-    if (human == 1) {
-        return "Rock"
-    } else if (human == 2) {
-        return "Paper"
-    } else {
-        return "Scissors"
+    if (h === c) { 
+        return "Tie!"
     }
+
+    if (
+        (h === "rock" && c === "scissors") ||
+        (h === "paper" && c === "rock") ||
+        (h ==="scissors" && c === "paper")
+    )   {
+        return 'Your win!'
+    }
+
+    if (h !== "rock" && h !== "paper" && h !== "scissors") {
+        console.log("Please enter something valid!")
+        let newChoice = getHumanChoice();
+        return playRound(newChoice, computer);
+    }
+
+    return "You lose:D"
 }
 
-console.log(getComputerChoice())
-console.log(getHumanChoice())
+console.log(playRound(humanChoice, computerChoice))
+console.log(`Player chose: ${humanChoice} | Computer chose: ${computerChoice}`);
 
