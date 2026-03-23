bc04cb3810932eefcc8419ed9a80eeebbde4e6ef
Ataraxia ‎ <gabrielvincentreyes13@gmail.com>
2026-03-23 11:04:23 +0800

    Added the prompt for player choice

--- javascript.js
+++ javascript.js
@@ -9,5 +9,18 @@ function getComputerChoice() {
     }
 }
 
+function getHumanChoice() {
+    let human = prompt("Whats your choice? (1)Rock, (2)Paper, or (3)Scissors?")
+
+    if (human == 1) {
+        return "Rock"
+    } else if (human == 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
+
 console.log(getComputerChoice())
+console.log(getHumanChoice())
 
