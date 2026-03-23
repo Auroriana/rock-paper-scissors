c336c6a9cd904749fc3cccb0039059fe1affe929
Ataraxia ‎ <gabrielvincentreyes13@gmail.com>
2026-03-23 11:00:09 +0800

    Added the randomizer for computer

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    i = Math.floor(Math.random() *3)+1
+    if (i == 1) {
+        return "Rock"
+    } else if (i == 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
+
+console.log(getComputerChoice())
+
